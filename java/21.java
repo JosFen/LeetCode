1c9a8fabc1f0d6b545b6d01aa84dc56e49739b56
Josceline <113638071+JosFen@users.noreply.github.com>
2024-10-07 17:49:43 -0700

    Update 21.java

--- java/21.java
+++ java/21.java
@@ -9,6 +9,7 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// Recursion:
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         if (list1 == null) return list2;
@@ -22,5 +23,31 @@ class Solution {
             list2.next = mergeTwoLists(list1, list2.next);
             return list2;
         }
+    }
+}
+
+// Iteration:
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+
+        ListNode dummy = new ListNode(-1, null);
+        ListNode curr = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                curr.next = list1;
+                list1 = list1.next;
+            } else {
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            curr = curr.next;
+        }
+        if (list1 == null) curr.next = list2;
+        else curr.next = list1;
+        
+        return dummy.next;
     }
 }
