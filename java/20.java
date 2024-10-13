29472377ce99263a6c8361fc641c97cd270a6522
Josceline <113638071+JosFen@users.noreply.github.com>
2024-10-14 07:06:50 +0800

    Update 20.java

--- java/20.java
+++ java/20.java
@@ -1,51 +1,70 @@
-// 2nd Version: using array
+// Use static array:
 class Solution {
     public boolean isValid(String s) {
-        int sLen = s.length();
-        if (sLen % 2 == 1) return false;
+        if (s.length() % 2 == 1) return false;
+        char[] stack = new char[s.length()];
 
-        char[] stack = new char[sLen];
-        int stackTopIdx = -1;
+        int top = -1;
         for (char c : s.toCharArray()) {
-            if (c == '(' || c == '[' || c == '{') {
-                stack[++stackTopIdx] = c;
-            } else if (c == ')' && (stackTopIdx == -1 || stack[stackTopIdx--] != '(')) {
-                return false;
-            } else if (c == ']' && (stackTopIdx == -1 || stack[stackTopIdx--] != '[')) {
-                return false;
-            } else if (c == '}' && (stackTopIdx == -1 || stack[stackTopIdx--] != '{')) {
-                return false;
+            if (c == '(' || c == '{' || c == '[') {
+                stack[++top] = c;
             }
+            else if (top >= 0) {
+                if ((c == ')' && stack[top] == '(')
+                 || (c == '}' && stack[top] == '{') 
+                 || (c == ']' && stack[top] == '[')) {
+                    top--;
+                 }
+                else return false;
+            }
+            else return false;
         }
-
-        return stackTopIdx == -1;
-
+        return (top == -1);
     }
 }
 
+// Use Stack() class:
+class Solution {
+    public boolean isValid(String s) {
+        if (s.length() % 2 == 1) return false;
 
-// // 1st Version: using Stack
-// class Solution {
-//     public boolean isValid(String s) {
-//         Stack<Character> stack = new Stack<>();
-//         int sLen = s.length();
+        Stack<Character> stack = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            if (ch == '(') stack.push(')');
+            else if (ch == '{') stack.push('}');
+            else if (ch == '[') stack.push(']');
+            else {
+                if (stack.empty() || stack.pop() != ch) return false;
+            }
+        }
 
-//         if (sLen % 2 == 1) return false;
-        
-//         String leftBrackets = "({[";
-//         String rightBrackets = ")}]";
-//         for (char c : s.toCharArray()) {
-//             if (leftBrackets.indexOf(c) > -1) stack.push(c);
-//             else {
-//                 if (stack.isEmpty()) return false;
-
-//                 char topChar = stack.peek();
-//                 if (leftBrackets.indexOf(topChar) == rightBrackets.indexOf(c)) stack.pop();
-//                 else return false;
-//             }
-//         }
-        
-//        return stack.isEmpty();
-
+//         return stack.empty();
 //     }
 // }
+
+
+// 1st Version: using Stack
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        int sLen = s.length();
+
+        if (sLen % 2 == 1) return false;
+        
+        String leftBrackets = "({[";
+        String rightBrackets = ")}]";
+        for (char c : s.toCharArray()) {
+            if (leftBrackets.indexOf(c) > -1) stack.push(c);
+            else {
+                if (stack.isEmpty()) return false;
+
+                char topChar = stack.peek();
+                if (leftBrackets.indexOf(topChar) == rightBrackets.indexOf(c)) stack.pop();
+                else return false;
+            }
+        }
+        
+       return stack.isEmpty();
+
+    }
+}
