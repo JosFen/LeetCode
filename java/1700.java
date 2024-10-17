6dd7acf6e758f298bd48d4e53125c50acb848408
Josceline <113638071+JosFen@users.noreply.github.com>
2024-10-17 10:34:36 +0800

    Create 1700.java

---
+++ java/1700.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int countStudents(int[] students, int[] sandwiches) {
+        int circleStudents = 0, squareStudents = 0;
+
+        for (int stu : students)  {
+            if (stu == 0) circleStudents++;
+            else squareStudents++;
+        }
+
+        for (int sw : sandwiches) {
+            if (sw == 0 && circleStudents == 0) return squareStudents;
+            if (sw == 1 && squareStudents == 0) return circleStudents;
+
+            if (sw == 0) circleStudents--;
+            else squareStudents--;
+        }
+
+        return 0;
+    }
+}
