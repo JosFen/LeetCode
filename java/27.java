1e42795b6d5550a3800965d1ea85c3a04247a8a1
Josceline <113638071+JosFen@users.noreply.github.com>
2024-10-17 11:20:03 +0800

    Create 27.java

---
+++ java/27.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int l = 0, r = 0;
+        
+        while (r < nums.length) {
+            if (nums[r] != val) nums[l++] = nums[r];
+            r++;
+        }
+        return l;
+    }
+}
