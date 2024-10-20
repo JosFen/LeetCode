28c25211f0397a9fcfe40c57b61f06dac338a88c
Josceline <113638071+JosFen@users.noreply.github.com>
2024-10-20 08:45:24 +0800

    Update 2.java

--- java/2.java
+++ java/2.java
@@ -39,17 +39,6 @@ class Solution {
     } 
 }
 
-// Recursion:
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 
 // Recursion: 
 class Solution {
