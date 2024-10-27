8e6d0b757352c75cc8cae27f487cb872fc8442b5
Josceline <113638071+JosFen@users.noreply.github.com>
2024-10-27 16:03:21 -0700

    Update 26.java

--- java/26.java
+++ java/26.java
@@ -1,13 +1,12 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         if (nums.length == 1) return 1;
-
-        int  l = 0, r = 1;
+        int l = 1, r = 1;
 
         while (r < nums.length) {
-            if (nums[r] != nums[l]) nums[++l] = nums[r];
+            if (nums[r] != nums[r-1]) nums[l++] = nums[r];
             r++;
-        } 
-        return l+1;
+        }
+        return l;
     }
 }
