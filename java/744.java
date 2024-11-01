0a7042aba6583638e40e2c2ac6b900b6263276fa
Josceline <113638071+JosFen@users.noreply.github.com>
2024-11-01 12:22:37 +0800

    Update 744.java

--- java/744.java
+++ java/744.java
@@ -1,3 +1,22 @@
+// Iteration: 
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int l = 0, r = letters.length - 1, m = 0;
+
+        while (l <= r) {
+            m = (l + r) / 2;
+            if (letters[m] <= target) l = m + 1; // increment l till letters[l]> target
+            else r = m - 1; // decrement r till letters[l] <=  target
+        }
+        /**
+        1. When all letters greater than target, return  l = 0 (r = -1 eventually)
+        2. When letters[m] == target, l = m + 1 && letters[l] > target
+        3. When all letters smaller than target, l == letters.length
+         */
+        return letters[l % letters.length]; 
+    }
+}
+
 // Recursion:
 class Solution {
     private int find(char[] letters, char target, int l, int r) {
