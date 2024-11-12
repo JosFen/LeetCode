c13c802b7c9f46110d623b901e55ae03f9827b3b
Josceline <113638071+JosFen@users.noreply.github.com>
2024-11-11 23:03:13 -0800

    Update 74.java

--- java/74.java
+++ java/74.java
@@ -1,3 +1,4 @@
+// use the upper right entry as the checkpoint
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int m = matrix.length;
@@ -8,6 +9,28 @@ class Solution {
             if (target == matrix[row][col]) return true;
             else if (target < matrix[row][col]) col--;
             else row++;
+        }
+        return false;
+    }
+}
+
+// Binary Search
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int l = 0, r = m * n;
+        while (l < r) {
+            int mid = (l + r) >>> 1;
+            int i = mid / n;
+            int j = mid % n;
+
+            if (matrix[i][j] == target) return true;
+            else if (matrix[i][j] > target) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
         }
         return false;
     }
